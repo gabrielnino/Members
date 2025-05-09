e98229dacc5f913fc5ae05b0e29c87cb37f8ec9a
gabrielnino <gabrielnino@gmail.com>
2025-05-09 11:56:44 -0700

    Refactor.

---
+++ Application/Common/Pagination/PagedResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common.Pagination
+{
+    /// <summary>
+    /// A page of items of type T plus the cursor to fetch the next page.
+    /// </summary>
+    public sealed record PagedResult<T>
+    {
+        /// <summary>
+        /// The items in the current page.
+        /// </summary>
+        public IEnumerable<T> Items { get; init; } = Array.Empty<T>();
+
+        /// <summary>
+        /// The opaque cursor to send back in the next request, or null if there is no more data.
+        /// </summary>
+        public string? NextCursor { get; init; }
+    }
+}
---
+++ Autodesk.Application/UseCases/CRUD/User/Query/IUserReadFilterCursor.cs
@@ -0,0 +1,14 @@
+﻿using Application.Common.Pagination;
+using Application.Result;
+
+namespace Autodesk.Application.UseCases.CRUD.User.Query
+{
+    using User = Domain.User;
+    public interface IUserReadFilterCursor
+    {
+        Task<Operation<PagedResult<User>>> ReadFilterCursor(
+            string name,
+            string? cursor,
+            int pageSize);
+    }
+}
