55ea6ede4360b54b624cdca79e020575979dc0b9
gabrielnino <gabrielnino@gmail.com>
2025-05-09 08:10:37 -0700

    Refactor.

--- Autodesk.Api/Migrations/20250509151009_InitialCreate.Designer.cs
+++ Autodesk.Api/Migrations/20250509151009_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Autodesk.Api.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20250509144818_InitialCreate")]
+    [Migration("20250509151009_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
