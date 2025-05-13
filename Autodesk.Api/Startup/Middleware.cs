16909e3b0aef435c17a0b7a0f1f7ceddf39a555f
gabrielnino <gabrielnino@gmail.com>
2025-05-13 11:20:49 -0700

    Add document links.

--- Autodesk.Api/Startup/Middleware.cs
+++ Autodesk.Api/Startup/Middleware.cs
@@ -5,12 +5,8 @@
         protected static void ConfigureMiddleware(WebApplication app)
         {
             app.UseCors(policyBuilder => policyBuilder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed(_ => true).AllowCredentials());
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
+            app.UseSwagger();
+            app.UseSwaggerUI();
             app.UseHttpsRedirection();
             app.UseAuthentication();
             app.UseAuthorization();
